eae7a0b0931923f0d07c77be32b1d744d6a29481
iwantmymtv <PTO@vestfrostsolutions.hu>
2023-01-25 16:49:10 +0100

    remove swr

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,17 @@
 import { useContext } from "react";
 import ContactList from "./components/ContactList/ContactList"
 import { ContactContext } from "./components/ContactList/ContactProvider"
-import useSWR from 'swr'
-
-const fetcher = (url:string) => fetch(url).then((res) => res.json())
 
 export default function Home() {
-  const {contacts, setContacts} = useContext(ContactContext);
-  const { data, error, isLoading } = useSWR('/api/contacts', fetcher)
-  setContacts(data)
+  const {contacts} = useContext(ContactContext);
+  let content = <ContactList data={contacts} /> 
+  if (contacts.length === 0){
+    content = <p className="h3 text-center mt-4">No contacts...</p>
+  }
 
   return (
     <main className="h-full mx-auto md:max-w-xl xl:max-w-5xl border-x border-grey-60">
-      
-        {isLoading && <p className="h3 text-center mt-4">Loading...</p>}
-        {data && <ContactList data={contacts} /> }
-        
+      {content}
     </main>
   )
 }
